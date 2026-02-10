e40d0fa33b23f47c8a90c2c61ecc7b8d6eda0b15
Maxence <maxence.torchin@etu.univ-amu.fr>
2026-02-10 18:17:08 +0100

    Ajout d'éléments de référencement

--- JAVASCRIPT/loader.js
+++ JAVASCRIPT/loader.js
@@ -32,22 +32,21 @@ window.addEventListener("load", () => {
     const phrases = [
         "De base, ce site était conçu uniquement pour une seule personne: Maxence TORCHIN.",
         "La version originale était bien différente de la version d'aujourd'hui.\nPas de changement de thèmes... Une seule page... juste une horloge et des cases pour des liens.",
-        "Cyril TAMINE a été la première personne à rejoindre le projet dans ses débuts, c'est même lui qui a initié la mise en page que vous voyez aujourd'hui.",
         "Monsieur Mickael Martin Nevot, aussi connu sous le nom de, \"LE GOAT\" a déjà réalisé un film.",
         "Conseil: N'arrivez pas en retard.",
         "Vraiment, ne venez pas en retard.",
         "Si tu vois ça, ta co est pas folle je crois.",
         "Savez vous qu'en contactant le créateur vous pouvez avoir votre propre site personnalisé? 50 Balles.\n\n(C'était une blague)",
         "Malgré les apparences, les emplois du temps ne sont pas raffraichis en temps réel. Ils sont actualisés au levé du soleil, à 12h15 et 20h00"
-      ];
-      
-      const texteElem = document.getElementById("loading-text");
-      if (texteElem) {
+    ];
+
+    const texteElem = document.getElementById("loading-text");
+    if (texteElem) {
         const phrase = phrases[Math.floor(Math.random() * phrases.length)];
         texteElem.textContent = phrase;
-      }
-      
-      
+    }
+
+
     setTimeout(() => {
         loadingScreen.style.transition = "opacity 0.3s ease-out";
         loadingScreen.style.opacity = "0";
